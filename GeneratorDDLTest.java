5501a2ddb2545076d9cee1a0a3e97ea9d7f3a120
haowei03151985 <zhuweirong2008@163.com>
2017-09-05 17:27:40 +0800

    添加DDL工具类

---
+++ GeneratorDDLTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.anxin.common.test.SpringTestCase;
+import com.anxin.common.util.generator.GeneratorDDLMySQL;
+import com.anxin.common.util.generator.GeneratorJavaBeanMySQL;
+import com.anxin.o2o.entity.Member;
+
+
+public class GeneratorDDLTest extends SpringTestCase{
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@Test
+	public void testGeneratorMapper() throws Exception {
+		Class<Member> clazz = Member.class;
+		GeneratorDDLMySQL.generatorMapper(clazz);
+	}
+
+	@Test
+	public void testGeneratorJavaBean() throws Exception {
+		GeneratorJavaBeanMySQL.generatorJavaBean(jdbcTemplate, "t_member", "Member");
+	}
+	
+}
